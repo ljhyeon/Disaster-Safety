cdf5cc4ad147cde13c8a05345be5710a0e10e073
ljhyeon <gee9328@gmail.com>
2025-06-30 23:51:42 +0900

    feat: 로그인 UI 구현

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,90 @@
-import { Layout, Button } from "antd";
+import { Layout, Typography, Input, Button, Form, Space, message } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { COLORS } from '../styles/colors'
+
+const { Content } = Layout
+const { Title, Text } = Typography
 
 const Login = () => {
+    const navigate = useNavigate()
+
+    const onLogin = async (values) => {
+        const { user_id, password } = values
+        try {
+            // ✅ 여기에 실제 로그인 API 요청 예정 - api 폴더에 구현해서 들고오기
+            // const response = await loginService.login({ username, password })
+            // if (response.success) {
+            //   navigate(`/home`)
+            // }
+
+            console.log('로그인 요청: ', values)
+            message.success('로그인 성공 (예시)')
+            navigate('/home')
+        } catch (error) {
+            message.error('로그인 실패: ', error);
+        }
+    }
+
     return (
-        <Layout style={{ minHeight: '100vh', width: '100vw' }}>
-            Login 페이지
-            <Button type="primary">Primary Button</Button>
-            <Button>Default Button</Button>
-            <Button type="dashed">Dashed Button</Button>
-            <Button type="text">Text Button</Button>
-            <Button type="link">Link Button</Button>
+        <Layout style={{ minHeight: '100vh', width: '100vw', backgroundColor: "white"}}>
+            <Content
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                }}
+            >
+                <div style={{ textAlign: 'center',}}>
+                    <Title level={1}>
+                        이어드림
+                    </Title>
+                    <Text type="secondary" style={{ display: 'block', marginBottom: 24 }}>
+                        재난상황 내 구호품 중계 플랫폼
+                    </Text>
+                </div>
+
+                <Form
+                    layout="vertical"
+                    onFinish={onLogin}
+                    autoComplete="off"
+                    style={{ width: '380px' }}
+                >
+                    <Form.Item
+                        label="아이디"
+                        name="user_id"
+                        rules={[{ required: true, message: '아이디를 입력하세요.' }]}
+                    >
+                    <Input placeholder="아이디" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="비밀번호"
+                        name="password"
+                        rules={[{ required: true, message: '비밀번호를 입력하세요.' }]}
+                    >
+                    <Input.Password placeholder="비밀번호" />
+                    </Form.Item>
+
+                    <Form.Item style={{ marginTop: '2rem', marginBottom: 0 }}>
+                        <Space style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Button
+                                style={{ backgroundColor: COLORS.primary, color: 'white' }}
+                                onClick={() => navigate('/signup')}
+                            >
+                                사용자 등록
+                            </Button>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                style={{ backgroundColor: COLORS.primary }}
+                            >
+                                로그인
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Content>
         </Layout>
     )
 }
