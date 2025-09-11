3731d677ee1cebd285bc9b14fa5d00eb6a880046
ljhyeon <gee9328@gmail.com>
2025-09-11 10:05:02 +1000

    feat: 기부 희망 물품 삭제 버튼 반영

--- web-app/src/components/setting/WishItem.jsx
+++ web-app/src/components/setting/WishItem.jsx
@@ -1,12 +1,15 @@
 import { Box, Typography, IconButton, } from '@mui/material';
-import { Delete as DeleteIcon } from '@mui/icons-material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const WishItem = ({ donation, handleDelete }) => {
 
     return (
         <Box key={donation.id} sx={{ borderRadius: '12px', border: '1px solid #F3F4F6', my: 1, }}>
             <Box sx={{ p: '19px', }}>
-                <Typography variant='caption'>식량 &gt; 즉석식품</Typography> {/* 수정 필요 */}
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
+                    <Typography variant='caption'>식량 &gt; 즉석식품</Typography> {/* 수정 필요 */}
+                    <IconButton onClick={() => handleDelete(donation.id)} sx={{ color: "#D8D8D8" }}><CloseIcon /></IconButton>
+                </Box>
                 <Typography variant="body1" sx={{ mb: 0.5 }}>{donation.item_name}</Typography>
                 <Typography variant='body2' color="#1428A0">수량: 20개</Typography> {/* 수정 필요 */}
             </Box>
